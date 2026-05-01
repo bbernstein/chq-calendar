3549c8905a06f9a174c45cf94a25d452ba7eaadf
bbernstein <bernard@thebernsteins.com>
2026-05-01 17:49:01 -0400

    Plan 1, Task 8: public library exports and ship schema to docs/

--- tools/publisher-format/src/index.ts
+++ tools/publisher-format/src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export type * from './types';
+export { FEED_JSON_SCHEMA } from './schema';
+export { validateFeed, type ValidateOptions } from './validateFeed';
+export { extractFromHtml, type ExtractOptions, type ExtractResult } from './extractFromHtml';
+export { loadReferences, type References, type CategoryRef, type VenueRef } from './references';
