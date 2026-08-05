395c781a0a030a8a8fda3919d982884178a1b219
bbernstein <bernard@thebernsteins.com>
2026-08-05 11:34:01 -0400

    fix(programs): grant deploy role UpdateFunctionCode on program-ingest Lambda (#165)

--- infrastructure/github-actions.tf
+++ infrastructure/github-actions.tf
@@ -46,7 +46,8 @@ resource "aws_iam_policy" "github_actions" {
           aws_lambda_function.sync_status.arn,
           aws_lambda_function.sync_list.arn,
           aws_lambda_function.publisher_ingest.arn,
-          aws_lambda_function.article_ingest.arn
+          aws_lambda_function.article_ingest.arn,
+          aws_lambda_function.program_ingest.arn
         ]
       },
       {
